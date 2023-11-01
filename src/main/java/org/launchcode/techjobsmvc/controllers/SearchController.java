f5ec716a988250f50bf3d62a67b2fc355f8add47
gomathiraja83 <gomathimurugan@gmail.com>
2023-10-31 19:42:40 -0500

    All tasks completed and got the results

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -29,12 +29,12 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-    @PostMapping (value = "jobs")
+    @PostMapping (value = "results")
         public String displaySearchResults
             (Model model, @RequestParam String searchType, @RequestParam String searchTerm)
         {
         ArrayList<Job> jobs;
-        if (searchType.equals("all") || searchTerm.isEmpty())
+        if (searchTerm.equals("all") || searchTerm.isEmpty())
         {
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
